af589624c57b4de46dd3935ce3b04e36064530fc
iq1 <seriousjonnik@gmail.com>
2016-11-15 19:04:50 +0200

    changes to conf

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 gunicorn -c /etc/gunicorn.d/hello.py hello:application
