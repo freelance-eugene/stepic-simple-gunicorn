e176e6b967e513f3ab94ca18418b1117e26e6ae7
iq1 <seriousjonnik@gmail.com>
2016-11-15 19:33:27 +0200

    final fixes

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,5 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
 sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-gunicorn -c /etc/gunicorn.d/hello.py hello:application
+gunicorn -c /etc/gunicorn.d/hello.py hello:application &
